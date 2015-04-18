db90cccb3d0d5f2a6174780fcd584dda2291e3c4
Nicholas Reich <nick@schoolph.umass.edu>
2015-04-17 20:30:05 -0400

    code from ww analysis.

--- assets/labs/wits-and-wagers-analysis-nick.R
+++ assets/labs/wits-and-wagers-analysis-nick.R
@@ -1,13 +1,23 @@
 ## wits and wagers analyses
-## Nick Reich
+## Nick Reich + class
+
+library(dplyr)
+
+dd <- tbl_df(dat)
+dd <- dd[-1,]
+
+dd_new <- sapply(dd[,c(-1, -8)], as.numeric)
+
+dd <- data.frame(name=dd$name, dd_new)
 
 ## plot raw data
-qplot(roundNum, correct, data=dat, group=name, geom=c("point", "smooth"), color=name, se=FALSE)
-qplot(roundNum, correct, data=dat, group=name, color=name, se=FALSE) + 
+qplot(round_number, correct, data=dd, group=name, geom=c("point", "smooth"), color=name, se=FALSE, method="lm")
+qplot(round_number, correct, data=dat, group=name, color=name, se=FALSE) + 
     stat_summary(aes(group = name), fun.y="mean", geom = "line", size=1)
 
+
 ## aggregate data
-adat <- aggregate(dat$correct, by=list(name=dat$name, round=dat$roundNum), FUN=mean)
+adat <- aggregate(dd$correct, by=list(name=dd$name, round=dd$round_number), FUN=mean)
 qplot(round, x, color=name, data=adat, geom=c("point", "line"))
 
 adat2 <- aggregate(adat$x, by=list(name=adat$name), 
@@ -15,10 +25,26 @@ adat2 <- aggregate(adat$x, by=list(name=adat$name),
 
 
 ## run some GEE models
-require(gee)
-m_ind <- gee(correct~roundNum, id=name, family=binomial, data=dat, corstr="independence")
-m_exch <- gee(correct~roundNum, id=name, family=binomial, data=dat, corstr="exchangeable")
-m_unstr <- gee(correct~roundNum, id=name, family=binomial, data=dat, corstr="unstructured")
+library(gee)
+m_ind <- gee(correct~round_number, id=name, family=binomial, data=dd, corstr="independence")
+m_exch <- gee(correct~round_number, id=name, family=binomial, data=dd, corstr="exchangeable")
+m_unstr <- gee(correct~round_number, id=name, family=binomial, data=dd, corstr="unstructured")
 summary(m_ind)$coef
 summary(m_exch)$coef
 summary(m_unstr)$coef
+
+library(lme4)
+mdl_re <- glmer(correct~ round_number + (1|name), family=binomial, data=dd)
+mdl_re_slope <- glmer(correct ~ round_number + (round_number|name), family=binomial, data=dd)
+
+res <- ranef(mdl_re)
+dotchart(as.matrix(res$name))
+
+
+dd$ci_std <- with(dd, (ci_upper-ci_lower)/true_value)
+p <- ggplot(dd, aes(x = ci_std, y = name, color=factor(round_number))) + 
+    geom_point() +
+    facet_wrap(~correct) +
+    ggtitle("Confidence Interval Ranges for Correct and Incorrect Responses") + xlim(0, 100) +
+    labs(x = "Confidence Interval Range", y = "Participant Initials")
+p
